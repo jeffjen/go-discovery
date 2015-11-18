a774f8455d94cf51fa5ef0520cf38d03c3b6ef5b
Jeffrey Jen <yihungjen@gmail.com>
2015-11-18 15:41:04 +0000

    NEW: helper function to report generic node info

---
+++ info/info.go
@@ -0,0 +1,60 @@
+package info
+
+import (
+	disc "github.com/jeffjen/go-discovery"
+
+	_ "github.com/Sirupsen/logrus"
+
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	VERSION = os.Getenv("VERSION")
+
+	BUILD = os.Getenv("BUILD")
+
+	NODE_NAME = os.Getenv("NODE_NAME")
+
+	NODE_REGION = os.Getenv("NODE_REGION")
+
+	NODE_AVAIL_ZONE = os.Getenv("NODE_AVAIL_ZONE")
+
+	NODE_PUBLIC_IPV4 = os.Getenv("NODE_PUBLIC_IPV4")
+
+	NODE_PRIVATE_IPV4 = os.Getenv("NODE_PRIVATE_IPV4")
+)
+
+type NodeInfo struct {
+	Version   string `json:"version"`
+	Build     string `json:"build"`
+	Node      string `json:"node"`
+	Region    string `json:"region"`
+	Zone      string `json:"avail_zone"`
+	Public    string `json:"public_ipv4"`
+	Private   string `json:"local_ipv4"`
+	Discovery string `json:"discovery"`
+	Hearbeat  string `json:"heartbeat"`
+	TTL       string `json:"ttl"`
+	Timestamp string `json:"current_time"`
+}
+
+func Info(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+	enc.Encode(NodeInfo{
+		Version:   VERSION,
+		Build:     BUILD,
+		Node:      NODE_NAME,
+		Region:    NODE_REGION,
+		Zone:      NODE_AVAIL_ZONE,
+		Public:    NODE_PUBLIC_IPV4,
+		Private:   NODE_PRIVATE_IPV4,
+		Discovery: disc.Discovery,
+		Hearbeat:  disc.Hearbeat.String(),
+		TTL:       disc.TTL.String(),
+		Timestamp: time.Now().Format(time.RFC3339),
+	})
+}
